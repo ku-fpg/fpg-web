621aa79e7ed81ed9c56646a6a212f10cf38a1303
Andy Gill <andygill@ku.edu>
2012-12-19 11:47:32 -0600

    Adding the ability to scrape our website.

--- Main.hs
+++ Main.hs
@@ -16,6 +16,8 @@ import qualified Control.Exception as E
 import System.Posix (getSymbolicLinkStatus, isDirectory)
 import Control.Arrow
 import Control.Applicative hiding ((*>))
+import Data.List
+import Data.Char
 
 import Text.XML.HXT.Parser.HtmlParsec
 import Text.XML.HXT.DOM.ShowXml
@@ -23,7 +25,8 @@ import Text.XML.HXT.DOM.TypeDefs
 
 import Language.KURE.Walker
 import Data.Tree.NTree.TypeDefs
-
+import Control.Concurrent
+import Control.Concurrent.MVar
 
 import qualified Language.KURE as KURE
 import Language.KURE hiding (apply)
@@ -82,14 +85,18 @@ main2 ["clean"] = do
            removeDirectoryRecursive build_dir
         return ()
 
-main2 ["import"] = shake shakeOptions { shakeVerbosity = Diagnostic } $ do
+main2 ["import"] = do
+        let start = ["import/Home.markdown"]
+        v <- newMVar start
+        shake shakeOptions { shakeVerbosity = Diagnostic } $ do
 
 
         action $ do
                 liftIO $ createDirectoryIfMissing True $ build_dir
-        -- What you need to import
-        want ["import/Research.markdown"]
+                liftIO $ createDirectoryIfMissing True $ "import"
 
+        -- What you need to import
+        want start   -- root
 
         "import//*.markdown" *> \ out -> do
                 let url = "http://ittc.ku.edu/csdl/fpg/" ++ dropDirectory1 (replaceExtension out "")
@@ -102,7 +109,7 @@ main2 ["import"] = shake shakeOptions { shakeVerbosity = Diagnostic } $ do
                                 tag == mkName "div" && cls == mkName "class"
                            _ -> False)
                 liftIO $ print 1
-                let res0 = fromKureM error $ KURE.apply (collectT $ q) () (XTrees contents)
+                let res0 = fromKureM error $ KURE.apply (collectPruneT $ q) () (XTrees contents)
                 liftIO $ print ("len",length res0)
                 let (XTree res:_) = res0
                 liftIO $ putStrLn $ (take 50 $ show res0)
@@ -111,6 +118,60 @@ main2 ["import"] = shake shakeOptions { shakeVerbosity = Diagnostic } $ do
                 liftIO $ print 3
                 system' "pandoc" ["-f","HTML","-t","markdown","-o",out,tmp_file]
 
+                -- now follow links
+                let q :: Translate () KureM (NTree XNode) [String]
+                    q = matchXTag (mkName "a") >>> (tagT p idR $ \ a b -> [a])
+
+                    p :: Translate () KureM [NTree XNode] String
+                    p = extractT (oneT (promoteT r) :: Translate () KureM XTree String)
+
+                    r :: Translate () KureM (NTree XNode) String
+                    r = matchXAttr (mkName "href") >>> xattrT s (\ attr sub -> sub)
+
+                    s :: Translate () KureM [NTree XNode] String
+                    s = extractT (allT (promoteT t) :: Translate () KureM XTree String)
+
+                    t :: Translate () KureM (NTree XNode) String
+                    t = textT idR $ \ txt _ -> txt
+
+                    {-
+                    (extractT (oneT (promoteT s) :: Translate () KureM XTree String))
+
+                    s :: Translate () KureM [NTree XNode] String
+                    s = pure "X" -- textT idR $ \ txt _ -> txt
+                    -}
+
+                let res0 :: [String] = fromKureM error $ KURE.apply (prunetdT (promoteT q)) () (XTrees contents)
+
+                let prefix = "/csdl/fpg/"
+                let res1 = [ drop (length prefix) x |  x <- res0, prefix `isPrefixOf` x ]
+
+                sequence_ [ liftIO $  createDirectoryIfMissing True $ "import" ++ takeDirectory1 x
+                          | x <- res1
+                          , '/' `elem` x
+                          , isUpper (head x)
+                          , not ("biblio" `isPrefixOf` x)
+                          , not ("node" `isPrefixOf` x)
+                          , not ("user" `isPrefixOf` x)
+                          , not ("book" `isPrefixOf` x)
+                          ]
+--                 liftIO $ createDirectoryIfMissing True $ "import"
+
+                let res2 = nub (filter (not . null) res1)
+                let res3 = [ "import/" ++ x ++ ".markdown" | x <- res2
+                               , x /= "front_page"
+                          , not ("biblio" `isPrefixOf` x)
+                          , not ("node" `isPrefixOf` x)
+                          , not ("user" `isPrefixOf` x)
+                          , not ("book" `isPrefixOf` x)
+                          ]
+                var <- liftIO $ takeMVar v
+                liftIO $ print var
+                liftIO $ putMVar v (out : var)
+                let res4 = [ x | x <- res3, not (x `elem` (out:var)) ]
+                liftIO $ print ("need",out,res4)
+                need res4
+                return ()
 
 main2 _ = putStrLn $ unlines
         [ "usage:"
@@ -174,10 +235,37 @@ instance Walker () XTree where
 allRL :: (Monad m) => Translate () m a b -> Translate () m [a] [b]
 allRL rr = translate $ \ c -> mapM (KURE.apply rr c)
 
+-- Rewrite in terms of tagT
 matchXTag :: Monad m => QName -> Rewrite c m (NTree XNode)
 matchXTag tag = acceptR $ \ e -> case e of
         (NTree (XTag tag' _) _) -> tag == tag'
         _ -> False
+
+matchXAttr :: Monad m => QName -> Rewrite c m (NTree XNode)
+matchXAttr tag = acceptR $ \ e -> case e of
+        (NTree (XAttr tag') _) -> tag == tag'
+        _ -> False
+
+matchXText :: Monad m => Rewrite c m (NTree XNode)
+matchXText      = acceptR $ \ e -> case e of
+        (NTree (XText _) _) -> True
+        _ -> False
+
+
+tagT :: (Monad m) => Translate c m [NTree XNode] a -> Translate c m [NTree XNode] b -> (a -> b -> x) -> Translate c m (NTree XNode) x
+tagT ta tb f = translate $ \ c -> \ case
+         (NTree (XTag tag attrs) rest) -> liftM2 f (KURE.apply ta c attrs) (KURE.apply tb c rest)
+         _ -> fail "not XTag"
+
+xattrT :: (Monad m) => Translate c m [NTree XNode] a -> (QName -> a -> x) -> Translate c m (NTree XNode) x
+xattrT ta f = translate $ \ c -> \ case
+         (NTree (XAttr attr) rest) -> liftM (f attr) (KURE.apply ta c rest)
+         _ -> fail "not XAttr"
+
+textT :: (Monad m) => Translate c m [NTree XNode] a -> (String -> a -> x) -> Translate c m (NTree XNode) x
+textT ta f = translate $ \ c -> \ case
+         (NTree (XText text) rest) -> liftM (f text) (KURE.apply ta c rest)
+         _ -> fail "not XText"
 
 --test = collectT (contextfreeT $ \ (
 
